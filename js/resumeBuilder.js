cd7af2cb3d2e2d6496799f27064367dfde66ee3d
anumsh <anumsharma09@gmail.com>
2017-04-18 23:39:49 -0700

    feat: Add replace function and datatypes and variable

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,29 @@
-// append function usage
-// $ is jquery , so I am appending my name with id-main
-$("#main").append("Anum Sharma");
+ /*
+
+1. append method:
+example :  $("#main").append("Anum Sharma");
+
+2. $ is jquery , so I am appending my name after the below div tag in index.html
+
+ <div id="main">
+    <div id="header" class="center-content clear-fix">
+      <ul id="topContacts" class="flex-box"></ul>
+  </div>
+3. data types and variables:
+var name="anum";
+var age =20;
+
+4. console.log("anum")  will display anum in console tab (developer tool)  for debugging purpose
+
+example2:  var name= "anum";
+		   console.log(name);
+
+4. replace method:
+example:   var email= "anum@udacity.com";
+		  var new_email= email.replace("udacity", "gmail");
+		   console.log(email);
+		   console.log(new_email);
+
+5. 
+
+*/
